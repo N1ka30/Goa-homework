029940cd3f2815b426d050dc4d3fe8d348c32902
Nikoloz <zurabianinikusha@gmail.com>
2026-02-21 18:56:38 +0400

    Add files via upload

---
+++ Level 02/classwork.py
@@ -0,0 +1,9 @@
+print(30+24)
+print(25-2.85)
+name="nikoloz"
+print(name)
+age=15
+print(age)
+color="red"
+print(color)
+print("bye group 92")
---
+++ Level 02/homework.py
@@ -0,0 +1,63 @@
+#integer არის მთელი რიცხვები მაგალითად 1-2-3-4-5-6 და ასე შემდეგ
+#string არის ტექსტი რომელიც ""-ში უნდა იყოს აუცილებლად მაგალითად "my name is nika"
+#float არის არაწესიერი რიცხვები მაგალითად 2.7- 10.0-21,8  და ასე შემდეგ
+#ცვლადი არიის ისეთი რამ სადაც შეგვიძლია შევინახოთ ნებისმიერი რამ მაგალითად ინფორმაცია და ასე შემდეგ
+#iintegers
+print(2026)
+print(2010)
+print(30)
+print(100002)
+print(183)
+#stirngs
+print("my name is nika")
+print("im 15years old")
+print("i live in Rustavi")
+print("i love football")
+print("i love goa")
+#float
+print(3.3)
+print(4.7)
+print (8.9)
+print(1.2)
+print(2.5)
+name="nika"
+age=15
+city="Rustavi"
+number=30
+#ცვლადებით აგებული წინადადება
+print("my name is "+name+"im 15" "i live in"+city)
+
+
+
+
+
+
+
+
+#მათემატიკური ოპერაციები 
+
+#გამოკლება
+print(100-3)
+print(100000_5000)
+print(30-30)
+#მიმატება
+print(30+130)
+print(100000+30000)
+print(908+92)
+#გაყოფა
+print(100/4)
+print(250/50)
+print(289/17)
+#გამრავლება
+print(10*100)
+print(10*89)
+print(89*5)
+#წინადადებების შეკრება
+print("my name is"+"nika")
+print("i am "+str(15)+" years old")
+print("i live in" + " Rustavi")
+#სიტყვის გამრავლება
+print("nika"*30)
+print('leave'*3)
+print("goa"*7)
+print(100+304+2000)
