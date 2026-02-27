f0249a554c7301b6d090bdc30a09d98195e4897d
Nikoloz <zurabianinikusha@gmail.com>
2026-02-27 18:55:59 +0400

    Add files via upload

---
+++ Level 03/homework.py
@@ -0,0 +1,38 @@
+#კომენტარი არის ტექსტი რომელსაც პაითონი არ ასრულებს იგი იმისთვის არის რომ გავიგოთ კოდი უკეთ გავიგოთ
+#case-sensetive პროგრამის ენაა არის რომელშიც მნიშვნელობა აქვს ასოების შრიფტს
+#case-insensetive აქ კი პირიქით არის ასოების შრიფთს არ აქვს მნიშვნელობა
+#snake case არის space პაითონში რადგან აქ არ შეიძლება space-ის გამოყენება და აღინიშნება _ით
+ 
+#snake case 
+favourite_movie = "granturismo"
+favourite_snack = "snickers"
+study_subject = "programing"
+favourite_day = "friday"
+mood_today = "happy"
+print("my favourite movie is " + favourite_movie + " my favourite snack is " + favourite_snack + " today i learned about " + study_subject + " today is " + favourite_day + " i feel " + mood_today )
+#bug არის შეცდომა პროგრამაში რის გამოც შეიძლება კოდმა არ იმუშავოს
+#debuging კი შეცდომის პოვნა და გასწორება არის
+
+# შეცდომის შესწორება 1 
+name="luka"
+print(name)
+#შეცდომის გასწორება 2
+number = "5"
+text = " apples"
+result = number + text
+print(result)
+#შეცდომის გასწორება 3
+name="lika"
+name2=name+"4"
+print(name2)
+#შეცდომის გასწორება 4
+first_user="lika"
+second_user= "giorgi"
+print(first_user)
+#შეცდომის გასწორება 5
+first_name="data"
+last_name="random"
+#შეცდომის გასწორება 6
+first_user="lika"
+second_user="giorgi"
+print(first_user)
