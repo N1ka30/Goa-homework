ea2819f8f344a58632413e8a54b17c0288e474ac
Nikoloz <zurabianinikusha@gmail.com>
2026-03-10 15:10:01 +0400

    Add files via upload

---
+++ level 04/classwork.py
@@ -0,0 +1,25 @@
+#დავალება 1
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+average = (num1 + num2 + num3) / 3 
+print(average)
+#დავალება 2
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+sum = num1 + num2
+print(sum)
+#დავალება 3
+my_name = input("Enter your name: ")
+mother = input("Enter your mother's name: ")
+father = input("Enter your father's name: ")
+print("My name is " + my_name + ", my mother's name is " + mother + " and my father's name is " + father + ".")
+#დავალება 4
+city = input("Enter your city: ")
+district = input("Enter your district: ")
+building = input("Enter your building number: ")
+floor = input("Enter your floor: ")
+print("Your full address is: " + city + ", " + district + ", Building " + building + ", Floor " + floor)
+#დავალება 5
+age = input("Enter your age: ")
+print("You are " + age + " years old")
