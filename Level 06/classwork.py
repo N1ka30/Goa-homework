552f70100ed39836c54b07e14301363bf3458e6d
Nikoloz <zurabianinikusha@gmail.com>
2026-03-21 18:59:20 +0400

    Add files via upload

---
+++ Level 06/classwork.py
@@ -0,0 +1,13 @@
+name=input("enter you name:")
+print(name)
+number_1=int(input("enter the number:"))
+number_2=int(input("enter the number:"))
+print(number_1+number_2)
+age=int(input("enter your age:"))
+print(type(age))
+a=3 #integer
+b=5.5 #float
+c="hello"#string
+print(type(a))
+print(type(b))
+print(type(c))
---
+++ Level 06/homework.py
@@ -0,0 +1,59 @@
+#ინპუთი არის ადამიანის მიერ შეყვანილი მონაცემები ან ინფორმაცია რომელიც კომპიუტერს მიეწოდება ერთ ერთი მაგალითი არის კლავიატურა მაუსი და ასე შემდეგ
+#აუტფუთი არის კომპიუტერის მიერ ნაჩვენები ინფორმაცია ერთ ერთი მაგალიტია მონიტორი სპიკერი პრინტერი და ასე შემდეგ
+
+#დავალება 3
+name=input("enter you name:")
+print(type(name))
+#დავალება 4
+name = "Nika"  #string    
+city = "Ristavi"   #string  
+car = "Mercedes"      #string    
+food = "Shaurma"     #string
+color = "Red" #string
+
+
+age = 16     #integer
+year = 2026  #integer
+score = 100  #integer
+level = 5     #integer
+coins = 250    #integer
+
+
+
+price = 19.99      #float  
+height = 1.75      #float  
+weight = 52.3      #float 
+temperature = 35.4   #float
+speed = 100.1     #float
+
+#დავალება 5
+name="Nika"
+age=16
+height=1.75
+print(type(name))
+print(type (age))
+print(type (height))
+#დავალება 6
+word_1=input("enter first word:")
+word_2=input("enter second word:")
+final= word_1+word_2
+print(final)
+#დავალება 7
+number_1=int(input("enter first number:"))
+number_2=int(input("enter second number:"))
+number_3=int(input("enter third number:"))
+number_4=int(input("enter fourth number:"))
+number_5=int(input("enter fifth number :"))
+avarage=(number_1+number_2+number_3+number_4+number_5)/5
+print(avarage)
+#დავალება 8
+name=input("enter you name:")
+Last_name=input("enter your last name:")
+age=input("enter your age")
+height=input("enter your height;")
+weight=input("enter your weight:")
+print("hi my name is " + name + " my last name is " + Last_name + " i am " + age + " years old "  " im " + height + " cm " +' im ' + weight + " kg ")
+
+
+
+
