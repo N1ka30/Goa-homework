a2d80bf458a80e4c7ce36d302e0055bcb360a1ff
Nikoloz <zurabianinikusha@gmail.com>
2026-05-30 15:58:12 +0400

    Add files via upload

---
+++ Level 12/classwork.py
@@ -0,0 +1,16 @@
+for i in range(100):
+    print(i)
+
+for i in range(100,201,5):
+    print(i)
+
+
+i=1
+while i<=10:
+    print(i)
+    i+=1
+
+i=0
+while i <=100:
+    print(i)
+    i+=2
---
+++ Level 12/homework.py
@@ -0,0 +1,49 @@
+#დავალება 2
+for i in range(0,38):
+    print(i)
+
+#დავალება 3
+for i in range(30,40):
+    print(i)
+
+#დავალება 4
+for i in range(12,120):
+    print(i)
+    
+#დავალება 5
+for i in range(18):
+    print("Nika")
+
+#დავალება 6
+for i in range(10,100,3):
+    print(i)
+
+#დავალება 7
+for i in range(1,31):
+    print(i,"მწვანე ვაშლი")
+
+#დავალება 8
+age=input("enter ur age:")
+for i in range(13):
+    print(age)
+
+#დავალება 9
+i=1
+while i <=10:
+    print("Nika")
+    i+=1
+
+#დავალება 10
+i=0
+while i<=100:
+    print(i)
+    i+=1
+
+#დავალება 11
+
+
+    
+ 
+
+
+
